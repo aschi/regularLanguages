90257c6fa223dcc39cc0232e66dfbd090162c28b
Adrian Schmid <adrian@debian>
2013-12-27 18:53:00 +0100

    updated all runners

--- src/main/java/ch/zhaw/regularLanguages/evolution/EAWithConsistentGlobalProblemSet.java
+++ src/main/java/ch/zhaw/regularLanguages/evolution/EAWithConsistentGlobalProblemSet.java
@@ -25,8 +25,6 @@ public class EAWithConsistentGlobalProblemSet<E extends EvolutionCandidate, PSI 
 		this.candidates = candidates;
 		this.maxFitness = problemSet.getProblemSet().size();
 		this.reference = reference;
-		
-		System.out.println("Max Fitness: " + maxFitness);
 	}
 	
 	public E getWinner(){
@@ -56,17 +54,11 @@ public class EAWithConsistentGlobalProblemSet<E extends EvolutionCandidate, PSI 
 		List <E>newList = null;
 		
 		A : while(cycle < cycleLimit && finalForm == false){
-			System.out.println("Cycle: "+cycle);
-			
 			//calculate fitness values
 			for(E c : candidates){
 				c.setFitness(c.fitness(problemSet));
 			}
-			System.out.println("fitness values calculated..");
-			
 			Collections.sort(candidates);
-			
-			System.out.println("fitness values sorted..");
 			
 			//take the fitter half and clone & mutate all elements and put them into a new list
 			
@@ -77,7 +69,6 @@ public class EAWithConsistentGlobalProblemSet<E extends EvolutionCandidate, PSI 
 			}
 			for(int i = 0;i < (candidates.size())/2;i++){
 				if(candidates.get(i).getFitness() == maxFitness){
-					System.out.println("Winner candidate found..checking it");
 					if(candidates.get(i).checkValidity(reference)){
 						winner = candidates.get(i);
 						finalForm = true;
@@ -86,7 +77,6 @@ public class EAWithConsistentGlobalProblemSet<E extends EvolutionCandidate, PSI 
 				}
 				if(candidates.get(i).getFitness() > maxC){
 					maxC = candidates.get(i).getFitness();
-					System.out.println("new maxC: " + maxC);
 				}
 				
 				newList.add(candidates.get(i)); //add old object
@@ -94,9 +84,6 @@ public class EAWithConsistentGlobalProblemSet<E extends EvolutionCandidate, PSI 
 				newList.add((E)candidates.get(i).cloneWithMutation()); //add mutated clone
 				//System.out.println("clone added");
 			}
-			
-			System.out.println("clear list & set newlist");
-			
 			//continue with new list
 			candidates.clear();
 			candidates.addAll(newList);
@@ -105,98 +92,4 @@ public class EAWithConsistentGlobalProblemSet<E extends EvolutionCandidate, PSI 
 		}
 		return cycle;
 	}
-	/*
-	@Override
-	public long startEvolution(long cycleLimit) {
-		long cycle = 0;
-		boolean finalForm = false;
-		List <E>newList = null;
-		
-		long countEq = 0;
-		long countNbig = 0;
-		long countIbig = 0;
-		
-		A : while(cycle < cycleLimit && finalForm == false){
-			System.out.println("Cycle: "+cycle);
-			
-			if(newList == null){
-				newList = new LinkedList<E>();
-			}else{
-				newList.clear();
-			}
-			
-			for(int i = 0;i < candidates.size();i+=2){
-				int n = (i <= 0 ? candidates.size()-1 : i-1);
-				
-				int fitnessI = candidates.get(i).fitness(problemSet);
-				int fitnessN = candidates.get(n).fitness(problemSet);
-				
-				if(fitnessI == maxFitness){
-					System.out.println("Winner candidate found..stresstesting it");
-					if(candidates.get(i).checkValidity(reference)){
-						winner = candidates.get(i);
-						finalForm = true;
-						break A;
-					}
-				}
-				if(fitnessN == maxFitness){
-					System.out.println("Winner candidate found..stresstesting it");
-					if(candidates.get(n).checkValidity(reference)){
-						winner = candidates.get(n);
-						finalForm = true;
-						break A;
-					}
-				}
-				
-				if(fitnessI > maxC){
-					maxC = fitnessI;
-					System.out.println("new maxC: " + maxC);
-				}
-				
-				if(fitnessN > maxC){
-					maxC = fitnessN;
-					System.out.println("new maxC: " + maxC);
-				}
-				
-				
-				int x = fitnessI - fitnessN;
-				if(x == 0){
-					countEq++;
-					//candidate n = candidate i
-					if(Math.random() < 0.5){
-						newList.add(candidates.get(i));
-						newList.add((E)candidates.get(i).cloneWithMutation());
-					}else{
-						newList.add(candidates.get(n));
-						newList.add((E)candidates.get(n).cloneWithMutation());						
-					}
-				}else if(x < 0){
-					countNbig++;
-					//candidate n > candidate i
-					newList.add(candidates.get(n));
-					newList.add((E)candidates.get(n).cloneWithMutation());
-				}else if(x > 0){
-					countIbig++;
-					//candidate i > candidate n
-					newList.add(candidates.get(i));
-					newList.add((E)candidates.get(i).cloneWithMutation());	
-				}
-			}
-			
-			System.out.println("clear list & set newlist");
-			candidates.clear();
-			candidates.addAll(newList);
-			
-			Collections.shuffle(candidates);
-			
-			cycle++;
-		}
-		
-		System.out.println("EQ:"+ countEq);
-		System.out.println("NBiger:"+ countNbig);
-		System.out.println("IBiger:"+ countIbig);
-		
-		return cycle;
-	}
-	*/
 }
--- src/main/java/ch/zhaw/regularLanguages/evolution/runners/ConsitentGlobalABABLanguage.java
+++ src/main/java/ch/zhaw/regularLanguages/evolution/runners/ConsitentGlobalABABLanguage.java
@@ -1,55 +1,103 @@
 package ch.zhaw.regularLanguages.evolution.runners;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
 import ch.zhaw.regularLanguages.evolution.starters.ConsistentGlobalProblemSetStarter;
+import ch.zhaw.regularLanguages.evolution.starters.EvolvingGlobalProblemSetStarter;
 import ch.zhaw.regularLanguages.graphicalOutput.GraphvizRenderer;
+import ch.zhaw.regularLanguages.helpers.Logger;
 
 public class ConsitentGlobalABABLanguage {
 
 	public static void main(String[] args) {
+		ConsistentGlobalProblemSetStarter starter = new ConsistentGlobalProblemSetStarter();
+		starter.initLanguage(new char[]{'a','b'}, 10, "[ab]*abab");
 		
-		int solutionFoundCount = 0;
-		int noSolutionfoundCount = 0;
-		long cycleTmp;
-		List<Long> noCycles = new LinkedList<Long>();
+		int solutionFoundCounter = 0;
+		int noSolutionFound = 0;
+		List<Long> cycleCount = new LinkedList<Long>();
+		long tmpCycle;
 		long timeStamp;
 		
-		ConsistentGlobalProblemSetStarter starter = new ConsistentGlobalProblemSetStarter();
-		starter.initLanguage(new char[]{'a','b'}, 15, "[ab]*abab");
+		int[] problemCount = new int[5];
+		int[] candidatesCount = new int[5];
+		int[] noCycles = new int[2];
 		
-		for(int i = 0; i < 100;i++){
-			timeStamp = System.currentTimeMillis();
+		problemCount[0] = 50;
+		problemCount[1] = 100;
+		problemCount[2] = 150;
+		problemCount[3] = 200;
+		problemCount[4] = 250;
+		
+		candidatesCount[0] = 50;
+		candidatesCount[1] = 100;
+		candidatesCount[2] = 150;
+		candidatesCount[3] = 200;
+		candidatesCount[4] = 250;
+		
+		noCycles[0] = 250;
+		noCycles[1] = 500;
+		
+		int pc = 0;
+		int cc = 0;
+		int nc = 0;
+		
+		for(int n = 0;n < 25;n++){
+			DateFormat df = new SimpleDateFormat("yyyy-MM-dd_HH_mm_ss");
 			
-			//init
-			starter.initProblems(200);
-			starter.initCandidates(200);
+			Logger l = new Logger("C_G_"+df.format(new Date()) +".log", true);
 			
-			//start evolution
-			cycleTmp = starter.startEvolution(1000);
+			pc = problemCount[n%5];
+			cc = candidatesCount[(int)Math.floor(n/5)];
+			nc = noCycles[1];
 			
-			System.out.println(i+": finished ("+ (System.currentTimeMillis()-timeStamp)/1000 + "s)");
+			l.log("Problem Count: " + pc);
+			l.log("CandidatesCount: " + cc);
+			l.log("Max Cycles: " + nc);
 			
-			if(starter.getWinner() != null){
-				noCycles.add(cycleTmp);
-				solutionFoundCount++;
-				System.out.println(i+": solution found");
-				//GraphvizRenderer.renderGraph(starter.getWinner().getObj(), "winner.svg");
-			}else{
-				noSolutionfoundCount++;
-				System.out.println(i+": no solution found");
+			
+			solutionFoundCounter = 0;
+			noSolutionFound = 0;
+			cycleCount = new LinkedList<Long>();
+			
+			for(int i = 0;i < 100;i++){
+				timeStamp = System.currentTimeMillis();
+				
+				starter.initProblems(pc);
+				starter.initCandidates(cc);
+				tmpCycle = starter.startEvolution(nc);
+				
+				l.log(i+": finished ("+ (System.currentTimeMillis()-timeStamp) + "ms, " + tmpCycle + "cycles)");
+				
+				if(starter.getWinner() != null){
+					solutionFoundCounter++;
+					cycleCount.add(tmpCycle);
+					l.log(i + ": Solution found.");
+				}else{
+					noSolutionFound++;
+					l.log(i + ": No solution found.");
+				}
 			}
+			
+			long max=0;
+			long min=10000;
+			long sum=0;
+			for(long no : cycleCount){
+				sum+=no;
+				max = (no > max ? no : max);
+				min = (no < min ? no : min);
+			}
+			
+			l.log("Solution Found: " +solutionFoundCounter);
+			l.log("Avg cycles: " + (cycleCount.size() > 0 ? sum/cycleCount.size() : '0'));
+			l.log("Max cycles: " + max);
+			l.log("Min cycles: " + min);
+			l.log("No solution found: "+noSolutionFound);
+			l.finish();
 		}
-		
-		long sum = 0;
-		for(long l : noCycles){
-			sum+=l;
-		}
-		double avg = sum/noCycles.size();
-		
-		System.out.println("Solution Found:" + solutionFoundCount + "(Average cycles: " + avg +")");
-		System.out.println("No solution Found:" + noSolutionfoundCount);
-
 	}
 }
